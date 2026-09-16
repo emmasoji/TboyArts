163a1e2cb132e4ae95a8940b78c40dc32c1fd774
Emmanuel Oluwasoji <emmasoji56@gmail.com>
2026-09-16 14:21:00 +0100

    Allow custom domain CORS

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://tboyarts.netlify.app",
+        "https://tboyarts.shop",
     ],
     allow_credentials=True,
     allow_methods=["*"],
